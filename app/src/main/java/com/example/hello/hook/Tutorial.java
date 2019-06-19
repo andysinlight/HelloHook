28f7d1763f8660f5383d4985612f657e747a02e7
wy110888@163.com <My@2110!>
2019-06-19 16:25:42 +0800

    ..

---
+++ app/src/main/java/com/example/hello/hook/Tutorial.java
@@ -0,0 +1,10 @@
+package com.example.hello.hook;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class Tutorial implements IXposedHookLoadPackage {
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
+}
